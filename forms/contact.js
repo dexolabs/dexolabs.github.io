2a24b6e74503294e01e93d6000325ef513c23481
dexolabs <46881518+dexolabs@users.noreply.github.com>
2021-12-15 00:02:51 +0530

    Update contact.js

--- forms/contact.js
+++ forms/contact.js
@@ -2,16 +2,26 @@ document.querySelector("#contactForm").addEventListener("submit", (e) => {
   e.preventDefault();
 
   // Inputs
-  let name = document.querySelector("#name").value;
-  let email = document.querySelector("#email").value;
-  let subject = document.querySelector("#subject").value;
-  let message = document.querySelector("#message").value;
+  let name = document.querySelector("#name");
+  let email = document.querySelector("#email");
+  let subject = document.querySelector("#subject");
+  let message = document.querySelector("#message");
 
   Email.send({
     SecureToken: "7fa25d0f-2377-449b-9574-4f4cb65aacdf",
     To: "rishisoni6071@gmail.com",
-    From: `${email}`,
-    Subject: `Customer Query: ${subject}`,
-    Body: `Hi,<br/>I am ${name}, <br/><br/>${message}`,
-  }).then((message) => alert(message));
+    From: `${email.value}`,
+    Subject: `Query from Portfolio: ${subject.value}`,
+    Body: `Hi,<br/>I am ${name.value}, <br/><br/>${message.value}`,
+  }).then((resMessage) => {
+    alert(resMessage);
+    if (resMessage.includes("OK")) {
+      [name.value, email.value, subject.value, message.value] = [
+        "",
+        "",
+        "",
+        "",
+      ];
+    }
+  });
 });
