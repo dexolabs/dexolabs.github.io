b26f18cbccf2817eb74933de1faaaaa93d121f0c
dexolabs <46881518+dexolabs@users.noreply.github.com>
2022-02-24 23:29:57 +0530

    Add files via upload

---
+++ assets/js/data.js
@@ -0,0 +1,429 @@
+/** Portfolio Projects Data */
+export var portfolioData = [
+  {
+    pid: "459592",
+    name: "E-Commerce Application (for Electronics)",
+    description: `Built a fully responsive Electronics E-Commerce React Application which is completely as a Full-Stack Application with Search, Filter, Cart, Checkout, Billing, Payment Gateway etc... <br/>
+    It is basically a JAMstack Application with no-code backend using Commerce.js.<br/>
+    For the sake of performance and user-experience, Pagination and Shimmer also implemented respectively.`,
+    demo_link: "https://rishi-commercejs.netlify.app",
+    source_code: "https://github.com/rishi6071/shopping-cart-react-app",
+    category_name: "REACTJS",
+    category_classname: "filter-react",
+    client: "Self-Project",
+    project_date: "August 26, 2021",
+    poster_path: "ecommerce-jamstack/00_Ecommerce_JamStack.png",
+    technologies: [
+      "React.js",
+      "JSX",
+      "Material-UI",
+      "Styled Components",
+      "Axios",
+      "ES6 JavaScript",
+      "Commerce.js",
+      "Stripe.js",
+    ],
+    homescreen_overview: `ecommerce-jamstack/Ecommerce_JamStack_Overview.png`,
+    youtube_overview: ``,
+    images: [
+      "ecommerce-jamstack/00_Ecommerce_JamStack.png",
+      "ecommerce-jamstack/Ecommerce_Desktop.png",
+      "ecommerce-jamstack/Ecommerce_Tablet.png",
+      "ecommerce-jamstack/Ecommerce_Phone.png",
+    ],
+  },
+  {
+    pid: "882710",
+    name: "Weather Forecast",
+    description: `Buid a Weather Forecast App which shows REAL-TIME current weather and complete weather for current day in the gap of 2 Hours. Additionally, it shows the Forecast of next 3 days as well. Searching Feature is there to check the weather of any city in the entire world. It is Fully Mobile Responsive and Interactive.<br/>
+    I have used React Library with Material-UI Components, Bootstrap-5 and used axios for the API Calls.`,
+    demo_link: "https://rishi-weatherapp.netlify.app",
+    source_code: "https://github.com/rishi6071/weather-react-app",
+    category_name: "REACTJS",
+    category_classname: "filter-react",
+    client: "Self-Project",
+    project_date: "Feburary 17, 2021",
+    poster_path: "weather-forecast/00_Weather_App.png",
+    technologies: ["React.js", "JSX", "CSS-3", "ES6 JavaScript", "Axios"],
+    homescreen_overview: `weather-forecast/Weather_Overview.png`,
+    youtube_overview: ``,
+    images: [
+      "weather-forecast/00_Weather_App.png",
+      "weather-forecast/Weather_Desktop.png",
+      "weather-forecast/Weather_Tablet.png",
+      "weather-forecast/Weather_Phone.png",
+    ],
+  },
+  {
+    pid: "365485",
+    name: "Tic-Tac-Toe Game",
+    description: `built a very beautiful and interactive Tic-Tac-Toe Game with complete functionality.<br/>
+    I have used Vanilla JavaScript to develop it and additionally used the Lottie Animation for Winner Celebration.`,
+    demo_link: "https://rishi-tictactoe.netlify.app",
+    source_code: "https://github.com/rishi6071/tic-tac-toe-game",
+    category_name: "JAVASCRIPT",
+    category_classname: "filter-javascript",
+    client: "Self-Project",
+    project_date: "June 26, 2021",
+    poster_path: "tic-tac-toe/00_tic_tac_toe.png",
+    technologies: ["HTML-5", "CSS-3", "Bootstrap-5", "ES6 JavaScript", "OOPS"],
+    homescreen_overview: `tic-tac-toe/00_tic_tac_toe.png`,
+    youtube_overview: ``,
+    images: [
+      "tic-tac-toe/00_tic_tac_toe.png",
+      "tic-tac-toe/TicTacToe_Desktop.png",
+      "tic-tac-toe/TicTacToe_Tablet.png",
+      "tic-tac-toe/TicTacToe_Phone.png",
+    ],
+  },
+  {
+    pid: "704312",
+    name: "Business Portfolio Frontend",
+    description: `Created a Portfolio Template which can be use for any Business Personality or Skillful employ to enhance their impression and provide all the necessary information about him such as his blogs, portfolio details, services he provides and contact information.<br/>
+    It is full Mobile Responsive from 4 inch. Mobile Device to 15.6 inch. Desktop. It has been tested on 20+ different resolution devices.`,
+    demo_link: "https://rishi6071.github.io/allaboutus",
+    source_code:
+      "https://github.com/rishi6071/frontend-designs/tree/master/general-portfolio-frontend",
+    category_name: "Web Design",
+    category_classname: "filter-web",
+    client: "Self-Project",
+    project_date: "December 23, 2019",
+    poster_path:
+      "business-portfolio-frontend/00_Business_Portfolio_Frontend.png",
+    technologies: ["HTML-5", "CSS-3", "Bootstrap-5", "JavaScript"],
+    homescreen_overview: `business-portfolio-frontend/Business_Overview.png`,
+    youtube_overview: ``,
+    images: [
+      "business-portfolio-frontend/00_Business_Portfolio_Frontend.png",
+      "business-portfolio-frontend/Business_Desktop.png",
+      "business-portfolio-frontend/Business_Tablet.png",
+      "business-portfolio-frontend/Business_Phone.png",
+    ],
+  },
+  {
+    pid: "785592",
+    name: "Cover Letter Automation",
+    description: `Created an Automation to automate engineer's hustle to generate cover letter manually for each and every time when applying in a company. <br />
+    With this Automation, User provides his own cover letter format with details but instead of personal details, he'll be mentioning some keywords which will be replaced by his original details. At the end, User will get the Cover Letter in PDF Format.<br />
+    <strong>Inputs:</strong> CompanyName, JobRole, Name, Email, Phone & Address.<br />
+    <strong>Prerequisite:</strong> User needs to set the DownloadPath where the pdf file will be download and IntendedPath where he want that pdf to be store.`,
+    demo_link: "Soon",
+    source_code: "",
+    category_name: "RPA Automation",
+    category_classname: "filter-rpa",
+    client: "Self-Project",
+    project_date: "October 07, 2021",
+    poster_path: "cover-letter-automation/00_CoverLetterAutomation.jpg",
+    technologies: [
+      "Robotic Process Automation",
+      "Automation Anywhere",
+      "MS-Word",
+    ],
+    homescreen_overview: ``,
+    youtube_overview: ``,
+    images: ["cover-letter-automation/00_CoverLetterAutomation.jpg"],
+  },
+  {
+    pid: "372598",
+    name: "Isohack2.O - A 36 Hour Hackathon",
+    description: `Created the interactive Hackathon Website for the College Event called ISOHACK 2.0 - A 36 Hours Hackathon.<br/>
+    I have used Vanilla JavaScript to develop it and used the Particles of Isohack Logo for the Interactivity in the Website.`,
+    demo_link: "https://www.isohack.in",
+    source_code: "https://github.com/rishi6071/Isohack2.O-Website",
+    category_name: "JAVASCRIPT",
+    category_classname: "filter-javascript",
+    client: "Self-Project",
+    project_date: "December 21, 2020",
+    poster_path: "isohack2.O/00_Isohack_Hackathon.png",
+    technologies: [
+      "HTML-5",
+      "CSS-3",
+      "Bootstrap-5",
+      "ES6 JavaScript",
+      "Particle.js",
+    ],
+    homescreen_overview: `isohack2.O/Isohack_Overview.png`,
+    youtube_overview: `<iframe class="youtube_demo" src="https://www.youtube.com/embed/Bwo92WlfbgM" title="ISOHACK" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`,
+    images: [
+      "isohack2.O/00_Isohack_Hackathon.png",
+      "isohack2.O/Isohack_Desktop.png",
+      "isohack2.O/Isohack_Tablet.png",
+      "isohack2.O/Isohack_Phone.png",
+      "isohack2.O/Isohack_Gallery.jpg",
+    ],
+  },
+  {
+    pid: "314802",
+    name: "Forex Currency Converter",
+    description: `Built a Forex Currency Converter App using React which provides the service to convert the amount from one Currency to another Currency with REAL-TIME Currency Rate.<br/>
+    It's just like Google's Provided Currency Converter with same UI and Functionality.`,
+    demo_link: "https://rishi-currencyconverter.netlify.app",
+    source_code:
+      "https://github.com/rishi6071/forex-currency-converter-react-app",
+    category_name: "REACTJS",
+    category_classname: "filter-react",
+    client: "Self-Project",
+    project_date: "June 26, 2021",
+    poster_path: "forex-currency-converter/00_forex_currency_converter.jpg",
+    technologies: [
+      "React.js",
+      "HTML-5",
+      "CSS-3",
+      "Bootstrap-5",
+      "ES6 JavaScript",
+      "Axios",
+    ],
+    homescreen_overview: `forex-currency-converter/Forex_Overview.png`,
+    youtube_overview: ``,
+    images: [
+      "forex-currency-converter/00_forex_currency_converter.jpg",
+      "forex-currency-converter/Forex_Desktop.png",
+      "forex-currency-converter/Forex_Phone.png",
+    ],
+  },
+  {
+    pid: "180197",
+    name: "Instagram Automation",
+    description: `Created a very useful Automation for Instagram users or influencers who want something to like or comment each & every post of a particular user. <br/>
+    With this Automation, user will give instagram username of other person and bot will open that profile and go through with each & every post to like. <br />
+    For Comments, we have another bot which takes same input as previous and there is a common dataset of comments (user can manipulate it), from that dataset comments will be taken randomly and start commenting all post of that user. <br />
+    <strong>Prerequisite:</strong> Instagram account of Bot Runner must be logged in.`,
+    demo_link: "Soon",
+    source_code: "",
+    category_name: "RPA Automation",
+    category_classname: "filter-rpa",
+    client: "Self-Project",
+    project_date: "Dec 15, 2021",
+    poster_path: "instagram-automation/00_Instagram_Automation.png",
+    technologies: ["Robotic Process Automation", "Automation Anywhere"],
+    homescreen_overview: ``,
+    youtube_overview: ``,
+    images: ["instagram-automation/00_Instagram_Automation.png"],
+  },
+  {
+    pid: "106448",
+    name: "Live Job Portal",
+    description: `Built a dark mode fully mobile responsive Job Portal Website with search functionality using JavaScript.<br/>
+    I have integrated the Jobs API for REAL-TIME Data and when real-time does not comes due to either requests excided or any other error, the Testing Data will be shown from an Static API.`,
+    demo_link: "https://rishi-jobs-portal.netlify.app",
+    source_code: "https://github.com/rishi6071/job-portal-website",
+    category_name: "JAVASCRIPT",
+    category_classname: "filter-javascript",
+    client: "Self-Project",
+    project_date: "June 20, 2021",
+    poster_path: "job-portal/00_job_portal.png",
+    technologies: [
+      "HTML-5",
+      "CSS-3",
+      "Bootstrap-5",
+      "ES6 JavaScript",
+      "Fetch-API",
+    ],
+    homescreen_overview: `job-portal/JobPortal_Overview.png`,
+    youtube_overview: ``,
+    images: [
+      "job-portal/00_job_portal.png",
+      "job-portal/JobPortal_Desktop.png",
+      "job-portal/JobPortal_Tablet.png",
+      "job-portal/JobPortal_Phone.png",
+    ],
+  },
+  {
+    pid: "433211",
+    name: "NEWS Website - World Today",
+    description: `Built a Fully Responsive NEWS App with REAL-TIME Data created using React and Material-UI Components which shows the News Articles of Current Day. I have attached the filter for Country & Category. There are 50+ Countries Options provided and for each country there will be 7 Categories. Since, Reader can read the News of around 50+ Countries and in various Categories.
+    Also, I have integrated the Pagination API for the better performance and loading time.<br/>
+    Additionally, I have integrated Most Popular News Source named BBC News such that Reader can read the Top Articles provided by BBC News as well.`,
+    demo_link: "https://www.youtube.com/watch?v=7NdFRNiQ_vg",
+    source_code: "https://github.com/rishi6071/news-react-app",
+    category_name: "REACTJS",
+    category_classname: "filter-react",
+    client: "Self-Project",
+    project_date: "April 13, 2021",
+    poster_path: "news-app/00_news_app.png",
+    technologies: [
+      "React.js",
+      "HTML-5",
+      "CSS-3",
+      "Bootstrap-5",
+      "ES6 JavaScript",
+      "Axios",
+      "Material-UI",
+    ],
+    homescreen_overview: `news-app/News_Overview.png`,
+    youtube_overview: `<iframe class="youtube_demo" src="https://www.youtube.com/embed/7NdFRNiQ_vg" title="NEWS" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`,
+    images: [
+      "news-app/00_news_app.png",
+      "news-app/News_Desktop.png",
+      "news-app/News_Tablet.png",
+      "news-app/News_Phone.png",
+    ],
+  },
+  {
+    pid: "175805",
+    name: "Book Store Frontend",
+    description: `Created an Online Books Store Mobile Responsive Website Template. There are about 13 Screens in that template and good enough to become an professional eCommercial Website.<br/>
+    It's a basic multi-purpose demo website which can be customized as per your needs, i.e., as a e-commerce site or as a general customer support site etc.`,
+    demo_link: "https://rishi6071.github.io/bethebest",
+    source_code:
+      "https://github.com/rishi6071/frontend-designs/tree/master/books-store-frontend",
+    category_name: "Web Design",
+    category_classname: "filter-web",
+    client: "Self-Project",
+    project_date: "Feburary 2, 2020",
+    poster_path: "bookstore-frontend/00_Bookstore_Frontend.png",
+    technologies: ["HTML-5", "CSS-3", "Bootstrap-4", "JavaScript"],
+    homescreen_overview: `bookstore-frontend/Bookstore_Overview.png`,
+    youtube_overview: ``,
+    images: [
+      "bookstore-frontend/00_Bookstore_Frontend.png",
+      "bookstore-frontend/Bookstore_Desktop.png",
+      "bookstore-frontend/Bookstore_Tablet.png",
+      "bookstore-frontend/Bookstore_Phone.png",
+    ],
+  },
+  {
+    pid: "438438",
+    name: "ILoveImg-ILovePdf Automation",
+    description: `Created a very useful Automation for Instagram users or influencers who want something to like or comment each & every post of a particular user. <br/>
+    With this Automation, user will give instagram username of other person and bot will open that profile and go through with each & every post to like. <br />
+    For Comments, we have another bot which takes same input as previous and there is a common dataset of comments (user can manipulate it), from that dataset comments will be taken randomly and start commenting all post of that user. <br />
+    <strong>Prerequisite:</strong> Instagram account of Bot Runner must be logged in.`,
+    demo_link: "Soon",
+    source_code: "",
+    category_name: "RPA Automation",
+    category_classname: "filter-rpa",
+    client: "Self-Project",
+    project_date: "Jan 02, 2022",
+    poster_path: "iloveimg-automation/00_ilove_automation.jpg",
+    technologies: ["Robotic Process Automation", "Automation Anywhere"],
+    homescreen_overview: ``,
+    youtube_overview: ``,
+    images: ["iloveimg-automation/00_ilove_automation.jpg"],
+  },
+  {
+    pid: "590629",
+    name: "Covid-19 Tracker",
+    description: `Built a COVID-19 Tracker App using React & Bootstrap-5 which shows REAL-TIME the Infected, Recovered, and Deaths Count of Patient in the entire world. The user can see the COVID-19 cases and status of any Country.<br/>
+    Users will be able to analyse the Comparison of Infected, Recovered, and Deaths Count through Graph and Bar Chart.`,
+    demo_link: "https://rishi-covidstatus.netlify.app",
+    source_code: "https://github.com/rishi6071/covid-19-tracker-react-app",
+    category_name: "REACTJS",
+    category_classname: "filter-react",
+    client: "Self-Project",
+    project_date: "Feburary 23, 2021",
+    poster_path: "covid19-tracker/00_Covid19_Tracker.jpg",
+    technologies: [
+      "React.js",
+      "HTML-5",
+      "CSS-3",
+      "Bootstrap-5",
+      "ES6 JavaScript",
+      "Axios",
+    ],
+    homescreen_overview: `covid19-tracker/Covid19_Overview.png`,
+    youtube_overview: ``,
+    images: [
+      "covid19-tracker/00_Covid19_Tracker.jpg",
+      "covid19-tracker/Covid19_Desktop.png",
+      "covid19-tracker/Covid19_Tablet.png",
+      "covid19-tracker/Covid19_Phone.png",
+    ],
+  },
+  {
+    pid: "221242",
+    name: "Coffee Shop Frontend",
+    description: `Created a Coffee Shop Webpage using Materialize CSS Framework in which beautyful coffee cards, parallax and lot more stuff..`,
+    demo_link: "https://rishi6071.github.io/coffeeking",
+    source_code:
+      "https://github.com/rishi6071/frontend-designs/tree/master/coffee-king-frontend",
+    category_name: "Web Design",
+    category_classname: "filter-web",
+    client: "Self-Project",
+    project_date: "March 16, 2020",
+    poster_path: "coffeeshop-frontend/00_Coffeeshop_Frontend.png",
+    technologies: ["HTML-5", "CSS-3", "MaterializeCSS", "JavaScript"],
+    homescreen_overview: `coffeeshop-frontend/Coffeeshop_Overview.png`,
+    youtube_overview: ``,
+    images: [
+      "coffeeshop-frontend/00_Coffeeshop_Frontend.png",
+      "coffeeshop-frontend/Coffeeshop_Desktop.png",
+      "coffeeshop-frontend/Coffeeshop_Tablet.png",
+      "coffeeshop-frontend/Coffeeshop_Phone.png",
+    ],
+  },
+];
+
+/** Technical Skills Data */
+export var skillsData = [
+  {
+    name: "HTML5",
+    progress: 100,
+  },
+  {
+    name: "CSS3",
+    progress: 90,
+  },
+  {
+    name: "JavaScript",
+    progress: 75,
+  },
+  {
+    name: "ReactJS",
+    progress: 80,
+  },
+  {
+    name: "Robotic Process Automation",
+    progress: 95,
+  },
+  {
+    name: "Python 3",
+    progress: 75,
+  },
+  {
+    name: "SQL/MySql",
+    progress: 80,
+  },
+  {
+    name: "Core Java",
+    progress: 75,
+  },
+];
+
+/** TESTIMONIALS Data */
+export var testimonialsData = [
+  {
+    name: `Anurag Das Gupta`,
+    role: `Python Developer`,
+    image_url: `Anurag_Dasgupta.jpeg`,
+    recommendation: `I Have been with Rishi from last 3 years in college .He is a keen learner & try to implement new
+    things , good in frontend technology and hope he will do more in future.`,
+  },
+  {
+    name: `Rishabh Vyas`,
+    role: `Android & IOT Developer`,
+    image_url: `Rishabh_Vyas.jpg`,
+    recommendation: `I know Rishi since 3 years. He is really cool. I learn many thing from him. He is going
+    well and keep rocking. Whenever I ask any query he always ready for help. Rishi and Me are like
+    childhood friends 😎 we have lots of memory. And I want to say keep doing this all activities 👏 and
+    All the Very Best for future endeavours.`,
+  },
+  {
+    name: `Pratiti Bera`,
+    role: `Frontend Developer`,
+    image_url: `pratiti_bera.jpeg`,
+    recommendation: `Rishi and I had worked together in Putatoe Technologies Pvt.Ltd. He was one of the most promising team
+    member. His sense of responsibility, approach towards work, dedication and quality of the work is all
+    so remarkable. Rishi can be an asset for any organisation he works with. Best wishes!!!`,
+  },
+  {
+    name: `Vishal Yadav`,
+    role: `ML/AI Developer`,
+    image_url: `Vishal_Yadav.jpg`,
+    recommendation: `Rishi's method of working is remarkable. He adheres to the development cycles properly allowing him to
+    work on projects properly and displays excellent communication skills which enhance overall team
+    communication and productivity. It was really nice working with you at the hackathon Rishi.`,
+  },
+];
+
+/** To Generate Project ID - https://numbergenerator.org/random-6-digit-number-generator */
